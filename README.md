55a4556d852f9562455353d621bf58ec2115e8ff
John Nguyen <johnky.nguyen@gmail.com>
2017-04-15 15:19:11 +0800

    Add properties file.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # spring-demo
 Spring Demo Web Application
+
+To run application
+mvn spring-boot:run
+
+Access: localhost:8080
--- src/main/java/my/example/spring/Application.java
+++ src/main/java/my/example/spring/Application.java
@@ -1,5 +1,6 @@
 package my.example.spring;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +16,9 @@ import org.springframework.context.annotation.Configuration;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+        //SpringApplication.run(Application.class, args);
     }
 }
